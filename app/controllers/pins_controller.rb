15545d1b07aabdfb018420f382c197f220c78b38
Connor <Conparkeapb@yahoo.com>
2015-06-09 01:03:52 -0500

    fixing heroku typo

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -45,7 +45,7 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    def current_user
+    def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Get your own pin, you ham sandwich!" if @pin.nil?
     end  
