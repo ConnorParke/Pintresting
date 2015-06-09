f1fca63802ba08baa82f72b57d23013092c82e97
Connor <Conparkeapb@yahoo.com>
2015-06-09 00:41:59 -0500

    Added pictures to app

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -47,11 +47,11 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice:"Get your own pin, you ham sandwich!" if @pin.nil?
+      redirect_to pins_path, notice: "Get your own pin, you ham sandwich!" if @pin.nil?
     end  
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
